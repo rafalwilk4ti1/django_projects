215bc27eae2fc224081b9b90fa29688c1de4ae46
rafalwilk4ti1 <rafalwilk3ti1@gmail.com>
2021-08-30 16:18:46 +0200

    Makeover of the main page, styled a catalog page

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -127,9 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+MEDIA_URL = '/images/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
